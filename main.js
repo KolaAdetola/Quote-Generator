1561ddc7b889e2874e6230b6c967a6c5b96cb432
kolawoleadetola630 <kolawoleadetola630@gmail.com>
2024-07-15 14:55:21 +0000

    updated quotes

--- main.js
+++ main.js
@@ -307,10 +307,10 @@ const quotes = [
             quote: "If you spend too much time thinking about a thing, you’ll never get it done.",
             person: "Bruce Lee",
         },
-//         {
-//             quote:"Life is like a box of chocolates. You never know what you’re going to get.",
-//             person:" Forrest Gump",
-//         },
+        {
+            quote:"Life is like a box of chocolates. You never know what you’re going to get.",
+            person:" Forrest Gump",
+        },
 //         {
 //             quote: "The quality of your life is directly related to how much uncertainty you can comfortably handle.",
 //             person: "Tony Robbins",
