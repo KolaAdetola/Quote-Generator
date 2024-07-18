097d1c530410cc2e9968f6027cd16c11c7d35d3d
kolawoleadetola630 <kolawoleadetola630@gmail.com>
2024-07-18 15:06:45 +0000

    updated quote generator

--- main.js
+++ main.js
@@ -475,6 +475,10 @@ const quotes = [
             quote: "Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma, which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice And most important, have the courage to follow your heart and intuition.",
             person: "Steve Jobs"
         },
+        {
+            quote: "Life is about moments; don’t wait for them, create them.",
+            person: "Tony Robbins"
+        },
     ]
 
     btn.addEventListener('click', () => {
