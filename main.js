d7fce623c71cd71f9279ffb48c47ded489002482
kolawoleadetola630 <kolawoleadetola630@gmail.com>
2024-07-22 10:42:20 +0000

    updated quote generator

--- main.js
+++ main.js
@@ -531,6 +531,10 @@ const quotes = [
             quote: "No one can compete with you on being you. Most of life is a search for who and what needs you the most.",
             person: "Naval Ravikant"
         },
+        {
+            quote: "Your clarity determines how successfully you walk this life.",
+            person: "Sadhguru"
+        },
     ]
 
     btn.addEventListener('click', () => {
